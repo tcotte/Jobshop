c29babd26f41b358980cbd884978b3db2a23e8b0
tcotte <tristan.cotte@outlook.fr>
2021-02-02 15:46:48 +0100

    create Gantt chart

--- script/main.py
+++ script/main.py
@@ -1,20 +1,32 @@
 import argparse
+import os
+import sys
 import numpy as np
-from schedule import *
+
+from script.schedule import df_ordered_repr, random_solution, compute_data, detailed_repr, df_detailed_repr, draw_gantt
 
 
 def main():
     parser = argparse.ArgumentParser()
-    DATA_DIR = "instances/"
-    parser.add_argument('--instance', type=str, default=DATA_DIR + "aaa1")
+    ROOT_DIR = sys.path[1]
+    DATA_DIR = ROOT_DIR + "/instances/"
+    parser.add_argument('--instance', type=str, default="aaa1")
     args = parser.parse_args()
 
-    nb_machines, nb_jobs, machines, durations = compute_data(args.instance)
+    nb_machines, nb_jobs, machines, durations = compute_data(DATA_DIR + args.instance)
 
-    list_job_operation = random_solution(nb_machines, nb_jobs, machines)
-    df = df_detailed_repr(list_job_operation, nb_machines)
-    print(df)
+    # list_job_operation = random_solution(nb_machines, nb_jobs, machines)
+    # Create solution which works
+    list_job_operation = [[(1, 1), (2, 2)], [(2, 1), (1, 2)], [(1, 3), (2, 3)]]
+    df_solution = df_ordered_repr(list_job_operation, nb_machines)
+    print(df_solution)
 
+    representation = detailed_repr(nb_jobs, nb_machines, df_solution, durations)
+    print(representation)
+    df_repr = df_detailed_repr(nb_jobs, representation)
+    print(df_repr)
+
+    draw_gantt(nb_jobs, nb_machines, machines, durations, representation)
 
 
 if __name__ == '__main__':
--- script/schedule.py
+++ script/schedule.py
@@ -2,8 +2,9 @@
 import argparse
 import numpy as np
 import pandas as pd
-from numpy.random import default_rng
-
+from datetime import date
+from datetime import timedelta
+from plotly.figure_factory import create_gantt
 
 parser = argparse.ArgumentParser()
 DATA_DIR = "instances/"
@@ -49,10 +50,8 @@ def random_solution(nb_machines, nb_jobs, machines):
     list_job_operation = []
 
     for n in range(nb_machines):
-        rng = default_rng()
-        list_ressource = rng.choice((1, nb_jobs), size=nb_jobs, replace=False).tolist()
+        list_ressource = np.random.permutation(np.arange(1, nb_jobs + 1)).tolist()
         list_solution.append(list_ressource)
-
 
     for i in range(len(list_solution)):
         list_tuples = []
@@ -66,8 +65,67 @@ def random_solution(nb_machines, nb_jobs, machines):
     return list_job_operation
 
 
-def df_detailed_repr(list_job_operation, nb_machines):
+def df_ordered_repr(list_job_operation, nb_machines):
     list_ressources = []
     [list_ressources.append("r" + str(i + 1)) for i in range(nb_machines)]
     return pd.DataFrame(list_job_operation, index=list_ressources)
 
+
+def index_col(t, df):
+    col = df.columns[df.isin([t]).any()].tolist()[0]
+    str_index = df.index[df[col].isin([t])].tolist()[0]
+    index = str_index.replace("r", "")
+    index = int(index)
+    return str_index, index - 1, col
+
+
+def detailed_repr(nb_jobs, nb_machines, df_solution, durations):
+    repr_job = np.zeros((nb_jobs, nb_machines))
+    counter_job = np.zeros(nb_jobs)
+    counter_ress = np.zeros(nb_machines)
+    ready_job = np.zeros(nb_jobs)
+    ready_ress = np.zeros(nb_machines)
+
+    while any(x != nb_jobs for x in counter_ress):
+        for job in range(1, nb_jobs + 1):
+            for op in range(1, nb_machines + 1):
+                t = (job, op)
+                ind, ressource, col = index_col(t, df_solution)
+
+                if (col == counter_ress[ressource]) & (op - 1 == counter_job[job - 1]):
+                    repr_job[job - 1][op - 1] = max(ready_job[job - 1], ready_ress[ressource])
+
+                    ready_ress[ressource] = ready_ress[ressource] + durations[job - 1, op - 1] + ready_job[job - 1]
+                    ready_job[job - 1] = repr_job[job - 1][op - 1] + + durations[job - 1, op - 1]
+
+                    counter_ress[ressource] += 1
+                    counter_job[job - 1] += 1
+
+    return repr_job
+
+
+def df_detailed_repr(nb_jobs, detailed_repr):
+    list_jobs = []
+    [list_jobs.append('J' + str(x)) for x in range(1, nb_jobs + 1)]
+    df_repr_detail = pd.DataFrame(data=detailed_repr, index=list_jobs, columns=range(1, 4))
+    return df_repr_detail
+
+
+def draw_gantt(nb_jobs, nb_machines, machines, durations, representation):
+    today = date.today()
+    # print(detailed_repr[0][0])
+    list_gantt = []
+    for i in range(nb_jobs):
+        for j in range(nb_machines):
+            list_gantt.append(['r' + str(machines[i, j] + 1),
+                               str(today + timedelta(days=representation[i][j])),
+                               str(today + timedelta(days=representation[i][j]) + timedelta(days=int(durations[i, j]))),
+                               'Job' + str(i + 1)])
+
+    df = pd.DataFrame(list_gantt,
+                      columns=['Task', 'Start', 'Finish', 'Resource'])
+
+    fig = create_gantt(df, index_col='Resource', show_colorbar=True,
+                       group_tasks=True)
+
+    fig.show()
